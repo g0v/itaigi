cb0c1514bebc3992d9d7832d9a72c099a11ce167
薛丞宏 <ihcaoe@gmail.com>
2015-04-01 21:17:18 +0800

    接好查請教條的關鍵字 丞宏

--- api/script.js
+++ api/script.js
@@ -5,37 +5,58 @@
 var app = angular.module('app',[]);
 
 app.config(function($httpProvider) {
-    //Enable cross domain calls
-    $httpProvider.defaults.useXDomain = true;
+		//Enable cross domain calls
+		$httpProvider.defaults.useXDomain = true;
 });
 
 app.controller("IntroController", 
-    ['$scope','$http', function($scope,$http){
-  $scope.questionList = [];
-  $scope.questionContent = [];
-  
-  $scope.info = [];
-  
-  $http.get(網址+'列表/外語請教條')
-    .success(function(data){
-    	console.log('外語請教條 success');
-      $scope.info=data;
-      $scope.questionList=data['列表'];
-  })
-  	.catch(function(data, status) {
-    console.error('外語請教條 error');
-  });
-  
-  $scope.viewQuestion= function(編號){
-	  $http.get(網址+'資料內容/'+編號)
-	    .success(function(data){
-	    	console.log('資料內容 success');
-		      $scope.info=data;
-		      $scope.questionContent=data;
-	  })
-	  	.catch(function(data, status) {
-	    console.error('資料內容 error');
-	  });
-  }
-  
+		['$scope','$http', function($scope,$http){
+			
+	$scope.info = [];
+	
+	$scope.listType='列表';
+	$scope.questionList = [];
+	$scope.suggestedQuestionList = [];
+	$scope.questionContent = [];
+	
+	$scope.keyword='美';
+	
+	$scope.showList=function(){
+		$http.get(網址+'列表/外語請教條')
+			.success(function(data){
+				console.log('外語請教條列表success');
+				$scope.listType='外語請教條列表';
+				$scope.info=data;
+				$scope.questionList=data['列表'];
+		})
+			.catch(function(data, status) {
+			console.error('外語請教條列表 error');
+		});
+	}
+
+	$scope.searchList=function(){
+		$http.get(網址+'揣/外語請教條',{params:{'關鍵字':$scope.keyword}})
+			.success(function(data){
+				console.log('揣外語請教條 success');
+				$scope.listType='搜尋結果';
+				$scope.info=data;
+				$scope.questionList=data['資料'];
+		})
+			.catch(function(data, status) {
+			console.error('揣外語請教條 error');
+		});
+	}
+	
+	$scope.viewQuestion= function(編號){
+		$http.get(網址+'資料內容/'+編號)
+			.success(function(data){
+				console.log('資料內容 success');
+					$scope.info=data;
+					$scope.questionContent=data;
+		})
+			.catch(function(data, status) {
+			console.error('資料內容 error');
+		});
+	}
+	
 }]);
