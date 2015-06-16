f391caec6ef2a71b3924bedb2859b3c61ab97e29
Pomin Wu <pomin5@gmail.com>
2015-06-16 11:58:51 +0800

    加 SearchBar 元件

--- mockup/src/SearchBar/SearchBar.jsx
+++ mockup/src/SearchBar/SearchBar.jsx
@@ -4,14 +4,25 @@ import React from 'react'
 import Transmit from 'react-transmit'
 
 class SearchBar extends React.Component {
+  state = {
+    q: this.props.q || ''
+  }
+
+  keyPress (evt) {
+    console.log(evt.key)
+    var q = this.state.q + evt.key
+    this.setState({q})
+  }
+
   render () {
     return (
         <div className='SearchBar'>
           <input type='text'
             placeholder='A... 怎麼講'
-            value={this.props.q}
-            />
-          <button className='SearchBar-submit'>找</button>
+            value={this.state.q}
+            onKeyPress={this.keyPress.bind(this)}/>
+          <button className='SearchBar-submit'
+            onSubmit={this.props.onSubmit}>找</button>
         </div>
       )
   }
