0b54ee53a17decdb8c9d5d72e3e35d065ee1e96a
Pomin Wu <pomin5@gmail.com>
2015-07-17 11:14:43 +0800

    add ABo component

--- mockup/src/ABo/ABo.jsx
+++ mockup/src/ABo/ABo.jsx
@@ -6,9 +6,16 @@ class ABo extends React.Component {
   render () {
     return (
         <div className='ui segment'>
-          <div className='abo ui action input'>
-            我要提供講法
+          <div className='abo ui input'>
+            <input placeholder='漢字' type='text'/>
           </div>
+          <div className='abo ui input'>
+            <input placeholder='台羅音標' type='text'/>
+          </div>
+          <div className='abo ui input'>
+            <input placeholder='提供者' type='text'/>
+          </div>
+          <button className='ui button'>送出</button>
         </div>
       )
   }
