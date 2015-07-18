8ba66317dbce6f954f6edea7afc0cb85d1f87005
Pomin Wu <pomin5@gmail.com>
2015-07-18 16:25:26 +0800

    add query to Kong component

--- mockup/src/Kong/Kong.jsx
+++ mockup/src/Kong/Kong.jsx
@@ -6,6 +6,7 @@ import SearchBar from '../SearchBar/SearchBar'
 import Su from '../Su/Su'
 import ABo from '../ABo/ABo'
 import TakKang from '../TakKang/TakKang'
+import superagent from 'superagent-bluebird-promise'
 
 class Kong extends React.Component {
   render () {
@@ -49,6 +50,8 @@ class Kong extends React.Component {
         </div>
       )
     }
+
+    const suList = this.props.suList.map((data) => <Su suData={data}/>)
     return (
         <div className='main container'>
           <nav className='navigation'>
@@ -60,8 +63,7 @@ class Kong extends React.Component {
           <div className='kong content'>
             <div className='ui su segment'>
               <div className='ui very relaxed list'>
-                <Su></Su>
-                <Su></Su>
+                {suList}
               </div>
             </div>
             <h3>啊無咧？</h3>
@@ -77,4 +79,11 @@ class Kong extends React.Component {
   }
 }
 
-export default Transmit.createContainer(Kong, {})
+export default Transmit.createContainer(Kong, {
+  queries: {
+    suList (queryParams) {
+      return superagent.get('http://127.0.0.1:8000/%E5%B9%B3%E8%87%BA%E9%A0%85%E7%9B%AE%E5%88%97%E8%A1%A8/%E6%8F%A3%E5%88%97%E8%A1%A8?%E9%97%9C%E9%8D%B5%E5%AD%97=一刀兩斷')
+        .then((res) => Promise.all(res.body['列表'].map((d) => Su.getQuery('suData', {suId: +d['外語項目編號']}))) )
+    }
+  }
+})
