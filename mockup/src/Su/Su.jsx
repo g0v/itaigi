351848301f7dd752553ce545e90b5c6040d75e84
Pomin Wu <pomin5@gmail.com>
2015-07-19 23:12:48 +0800

    fix bug
    check API returned data

--- mockup/src/Su/Su.jsx
+++ mockup/src/Su/Su.jsx
@@ -8,30 +8,33 @@ class Su extends React.Component {
   componentWillMount () { this.props.setQueryParams(this.props) }
   render () {
 
-    const suId = 192
-    const suText = '一屑仔'
-    const suData = {
-      "收錄者": "1",
-      "著作所在地": "臺灣",
-      "語言腔口": "閩南語",
-      "種類": "字詞",
-      "版權": "會使公開",
-      "來源": "2",
-      "推薦用字": "否",
-      "著作年": "2014",
-      "屬性內容":
-      {
-        "音標": "tsi̍t-sut-á"
-      },
-      "收錄時間": "2015-07-18 14:27:11"
-    }
+    //const suId = 192
+    //const suText = '一屑仔'
+    //const suData = {
+      //"收錄者": "1",
+      //"著作所在地": "臺灣",
+      //"語言腔口": "閩南語",
+      //"種類": "字詞",
+      //"版權": "會使公開",
+      //"來源": "2",
+      //"推薦用字": "否",
+      //"著作年": "2014",
+      //"屬性內容":
+      //{
+        //"音標": "tsi̍t-sut-á"
+      //},
+      //"收錄時間": "2015-07-18 14:27:11"
+    //}
 
-    //const suData = this.props.suData
+    const suId = this.props.suId
+    const suText = this.props.suText
+    const suData = this.props.suData
+
     return (
         <div className='su item'>
           <div className='content'>
             <a className='header'>{suText}</a>
-            <div className='content'>{suData['屬性內容']['音標']}</div>
+            <div className='content'>{suData['屬性內容'] ? suData['屬性內容']['音標'] : ''}</div>
             <div className='list'>
               <div className='item'>
                 <i className='right triangle icon'></i>
@@ -55,6 +58,7 @@ export default Transmit.createContainer(Su, {
       if (! suId) {
         return Promise.all([])
       }
+      console.log(suId)
       return superagent.get('http://127.0.0.1:8000/%E5%B9%B3%E8%87%BA%E9%A0%85%E7%9B%AE/%E7%9C%8B%E8%A9%B3%E7%B4%B0%E5%85%A7%E5%AE%B9?%E5%B9%B3%E8%87%BA%E9%A0%85%E7%9B%AE%E7%B7%A8%E8%99%9F=' + suId)
         .then((res) => res.body, (err) => console.log(err))
     }
