61b4f6f5bb4185289ab80d58b815242dde74c6c3
Pomin Wu <pomin5@gmail.com>
2016-01-07 20:06:11 +0800

    fix webpack settings

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -17,4 +17,31 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      loaders: ['babel', 'strict'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!postcss-loader'
+    },
+    {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'url-loader?limit=1'
+    },
+    {
+      test: /\.json$/,
+      loader: 'json-loader'
+    },
+    {
+      test: /\.html$/,
+      loader: 'html-loader'
+    }
+    ]
+  }
 };
