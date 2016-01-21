4e864f9d8026a8e93c201652bda2870b158c34aa
薛丞宏 <ihcaoe@gmail.com>
2016-01-21 04:07:58 -0500

    更新套件。而且配合django，改網址格式

--- server-side/itaigi/urls.py
+++ server-side/itaigi/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.static import serve
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^', include('臺灣言語平臺.網址')),
-    url(r'^影音檔案/(?P<path>.*)$', 'django.views.static.serve', {
+    url(r'^影音檔案/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
     }),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
