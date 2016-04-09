311a388fe583bcc142dfc42f20638f2f2cb3d582
mhshih <d00142002@ntu.edu.tw>
2016-04-10 00:31:10 +0800

    Issue #34 solved: FB.api(/postID?fiel         ds=source)

--- src/FBTest/FBTest.jsx
+++ src/FBTest/FBTest.jsx
@@ -6,50 +6,24 @@ import ToLam from '../GuanKiann/ToLam/ToLam';
 
 class FBTest extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: '',
-      name_album: '',
-      str: '',
-    };
-  }
-
   componentDidMount() {
     var _this = this;
     window.fbAsyncInit = function() {
       FB.init({
-        appId: '612719925532636',
+        appId: '252264671778664',
         xfbml: true,
         version: 'v2.5',
       });
 
-      FB.api({
-        method: 'fql.query',
-        query: 'SELECT object_id, name FROM album WHERE owner="1661026360846388"',
-      },
-        function(albums) {
-          if (albums && !albums.error) {
-            var images = [];
-            var name_album = albums[0].name;
-            FB.api({
-              method: 'fql.query',
-              query: 'SELECT object_id, src_big FROM photo WHERE album_object_id=' + albums[0].object_id,
-            },
-              function(photos) {
-                photos.forEach(function(item) {
-                  item.name_album = name_album;
-                  return item;
-                });
+      FB.getLoginStatus(function(response){
+        FB.api('/ukauitaigi/feed',function(feed){
+          var postID=feed.data[0].id.split('_')[1];
+          FB.api('/'+postID+'?fields=source',function(photo){
+            _this.getFacebookResponse(photo);
+          });
+        });
+      });
 
-                _this.getFacebookResponse(photos);
-              });
-          } else {
-            _this.getFacebookResponse.bind(_this);
-          }
-
-        }.bind(_this)
-      );
     }.bind(this)
 
     ;(function(d, s, id) {
@@ -67,25 +41,15 @@ class FBTest extends React.Component {
     return (
     <div className='kong content'>
       <div className='ui segment'>
-        <h3>Facebook Image ({this.state.name_album})</h3>
-        <img src={this.state.src} />
+        <h3>Facebook Image</h3>
+        <a href={this.state.href}><img src={this.state.src} /></a>
       </div>
     </div>
     );
   }
 
   getFacebookResponse(photo) {
-    if (photo && !photo.error) {
-      this.setState({ status: 'right' });
-      this.setState({ name_album: photo[0].name_album });
-      this.setState({ src: photo[0].src_big });
-    } else {
-      console.log(response);
-      this.setState({ status: 'fail' });
-      this.setState({ name_album: 'Error' });
-      this.setState({ str: JSON.stringify(response) });
-    }
-
+    this.setState({href:'http://facebook.com/'+photo.id,src:photo.source});
   }
 
   render() {
