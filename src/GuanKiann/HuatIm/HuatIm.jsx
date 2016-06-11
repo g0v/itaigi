2a21c211cc538a941a20781cd73a3911ab7b2b69
Pomin Wu <pomin5@gmail.com>
2016-06-11 16:08:57 +0800

    修好 #62 如果沒有音檔就不顯示發音按鈕

---
+++ src/GuanKiann/HuatIm/HuatIm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Transmit from 'react-transmit';
+import Promise from 'bluebird';
+var superagent = require('superagent-promise')(require('superagent'), Promise);
+import SoundsMapping from './SoundsMapping';
+import Debug from 'debug';
+
+var debug = Debug('itaigi:HuatIm');
+
+export default class HuatIm extends React.Component {
+  render() {
+    if (!this.props.suData.屬性內容.音標) {
+      return <div className='HuatIm hidden'></div>;
+    }
+
+    if (SoundsMapping.map(this.props.suData.屬性內容.音標) === undefined) {
+      return <div className='HuatIm hidden'></div>;
+    }
+
+    return (
+      <audio controls>
+        <source src={'http://t.moedict.tw/' + SoundsMapping.map(this.props.suData.屬性內容.音標) + '.ogg'} type='audio/ogg' />
+      </audio>
+    );
+  }
+};
--- src/GuanKiann/Su/Su.jsx
+++ src/GuanKiann/Su/Su.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Transmit from 'react-transmit';
 import LaiLik from '../LaiLik/LaiLik';
+import HuatIm from '../HuatIm/HuatIm';
 import Promise from 'bluebird';
 var superagent = require('superagent-promise')(require('superagent'), Promise);
-import SoundsMapping from './SoundsMapping';
 import Debug from 'debug';
 
 var debug = Debug('itaigi:Su');
@@ -33,9 +33,7 @@ class Su extends React.Component {
         <div className='content'>
           {suData.屬性內容 ? suData.屬性內容.音標 : ''}
         </div>
-        <audio controls>
-          <source src={"http://t.moedict.tw/" + SoundsMapping.map(suData.屬性內容.音標 ? suData.屬性內容.音標 : '') + ".ogg"} type='audio/ogg' />
-        </audio>
+          <HuatIm suData={suData} />
         <div className='list'>
           <div className='item'>
             <i className='right triangle icon'></i>
