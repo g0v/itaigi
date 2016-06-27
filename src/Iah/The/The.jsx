0067ec9cbf2930b7841355904bf57c73a46eacd5
薛丞宏 <ihcaoe@gmail.com>
2016-06-28 07:15:20 +0800

    「我很會」接後端 #79

--- src/Iah/The/The.jsx
+++ src/Iah/The/The.jsx
@@ -3,6 +3,7 @@ import Transmit from 'react-transmit';
 import { Link } from 'react-router';
 import Tshue from '../../GuanKiann/Tshue/Tshue';
 import ABo from '../../GuanKiann/ABo/ABo';
+var superagent = require('superagent-promise')(require('superagent'), Promise);
 
 class The extends React.Component {
   propTypes = {
@@ -11,15 +12,18 @@ class The extends React.Component {
     handleKong: React.PropTypes.func,
   };
 
+  componentWillMount() { this.props.setQueryParams(this.props); }
+
   render() {
     if (!this.props.params.k) {
+      let 無建議的外語列表=this.props.外語列表.列表.map((guaGi)=>(<div>{guaGi.外語資料}</div>))
       return (
       <div className='main container'>
         <div className='the content'>
           <div className='ui forum segment'>
             <h3>這些詞還沒有人會用台語講</h3>
             <div>
-              la-baar
+              {無建議的外語列表}
             </div>
             <h3>我就是沒有人，我來講</h3>
             <ABo 後端網址={this.props.後端網址} />
@@ -56,4 +60,17 @@ class The extends React.Component {
   }
 }
 
-export default Transmit.createContainer(The, {});
+export default Transmit.createContainer(The, {
+  queries: {
+    外語列表 ({ 後端網址 }) {
+      return superagent.get(後端網址 + '平臺項目列表/揣無建議的外語')
+        .then(({ body }) => (body))
+      .catch((err) => ({
+        '結果': -1,
+        '訊息': '發生錯誤',
+        '內容': err,
+      }));
+    },
+  },
+});
+
