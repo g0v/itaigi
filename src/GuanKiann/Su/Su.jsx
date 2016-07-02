5691641bdf87ce39713181ffd09d838b5c35fbbf
thewayiam <twly.tw@gmail.com>
2016-07-02 18:08:15 +0800

    #84: 一個詞條一人投一票

--- src/GuanKiann/Su/Su.jsx
+++ src/GuanKiann/Su/Su.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Transmit from 'react-transmit';
+import cookie from 'react-cookie';
 import LaiLik from '../LaiLik/LaiLik';
 import HuatIm from '../HuatIm/HuatIm';
 import TuiIngHuaGi from './TuiIngHuaGi';
@@ -16,6 +17,7 @@ class Su extends React.Component {
     this.state = {
       按呢講好: props.suData.按呢講好,
       按呢無好: props.suData.按呢無好,
+      voted: cookie.load('vote_'+props.suId),
     };
   }
 
@@ -29,6 +31,10 @@ class Su extends React.Component {
   }
 
   投票(evt) {
+    if (cookie.load('vote_'+this.props.suId)) {
+        alert('這句投過了!');
+        return
+    }
     var 票 = {
       '平臺項目編號': this.props.suId,
       'decision': evt,
@@ -38,23 +44,34 @@ class Su extends React.Component {
       .set('Content-Type', 'application/x-www-form-urlencoded')
       .set('X-CSRFToken', this.props.csrftoken)
       .send(票)
-      .then(({body}) => {if (body.success) console.log('succeed: '+body.suId)})
+      .then(({body}) => {if (body.success) cookie.save('vote_'+body.suId, evt, { path: '/' });})
       .catch(res => {
         console.log(res);
       });
     if (evt === '按呢講好')
-      this.setState({按呢講好: (this.state.按呢講好 || this.props.suData.按呢講好)+1});
+      this.setState({
+        按呢講好: (this.state.按呢講好 || this.props.suData.按呢講好)+1,
+        voted: evt
+      });
     else if (evt === '按呢無好')
-      this.setState({按呢無好: (this.state.按呢無好 || this.props.suData.按呢無好)+1});
+      this.setState({
+        按呢無好: (this.state.按呢無好 || this.props.suData.按呢無好)+1,
+        voted: evt
+      });
   }
 
   render() {
-    const { suText, suIm, suData, 後端網址 } = this.props;
+    const { suText, suIm, suId, suData, 後端網址 } = this.props;
     debug(this.props);
     if (suData.結果 == -2) {
       return <div className='su item'></div>;
     }
     let 按呢講的外語=this.props.按呢講的外語列表.map((外語)=>(<TuiIngHuaGi 外語={外語}/>))
+    var voted = function(suId) {
+        if (cookie.load('vote_'+suId))
+          return "disabled";
+        return "";
+    }
     return (
     <div className='su card'>
       <div className='content'>
@@ -75,8 +92,8 @@ class Su extends React.Component {
           <div className='item'>
             <i className='thumbs outline up icon'></i>
             <div className="content">
-              按呢講好 ({this.state.按呢講好 || suData.按呢講好})
-              <div className="ui button left pointing label green" onClick={this.投票.bind(this, '按呢講好')}>
+              按呢講好<span className="ui yellow circular label">{this.state.按呢講好 || suData.按呢講好}</span>
+              <div className={"ui button left pointing label green "+(voted(suId) || this.state.voted ? "disabled" : "")} onClick={this.投票.bind(this, '按呢講好')}>
               +1
               </div>
             </div>
@@ -84,8 +101,8 @@ class Su extends React.Component {
           <div className='item'>
             <i className='thumbs outline down icon'></i>
             <div className="content">
-              按呢無好 ({this.state.按呢無好 || suData.按呢無好})
-              <div className="ui button left pointing label teal" onClick={this.投票.bind(this, '按呢無好')}>
+              按呢怪怪<span className="ui orange circular label">{this.state.按呢無好 || suData.按呢無好}</span>
+              <div className={"ui button left pointing label teal "+(voted(suId) || this.state.voted ? "disabled" : "")} onClick={this.投票.bind(this, '按呢無好')}>
               +1
               </div>
             </div>
