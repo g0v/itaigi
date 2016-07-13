2cc5100d64202608775ef7772943e516e3969c5d
thewayiam <twly.tw@gmail.com>
2016-07-13 15:05:29 +0800

    decodeURI on disqus url for consistent

--- src/Disqus/Disqus.jsx
+++ src/Disqus/Disqus.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Transmit from 'react-transmit';
 import ReactDisqusThread from 'react-disqus-thread';
+import Debug from 'debug';
+
+var debug = Debug('itaigi:Disqus');
 
 class Disqus extends React.Component {
 
   render() {
     const { pathname } = this.props.location;
+    debug('pathname: ' + pathname);
+    debug('decode pathname: ' + decodeURI(pathname));
     return (
       <ReactDisqusThread
         shortname='itaigi'
-        title={pathname}
-        url={'http://itaigi.tw' + pathname}/>
+        title={decodeURI(pathname)}
+        url={'http://itaigi.tw' + decodeURI(pathname)}/>
     );
     }
 }
