b7d4dcc693fceb3618042ed3f90b0f0137177fec
jojomango <q9898989@gmail.com>
2016-07-23 16:56:43 +0800

    setup content

--- src/Iah/Iong/Iong.jsx
+++ src/Iah/Iong/Iong.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router';
+import {list} from './config';
 
 export default class Iong extends React.Component {
   render() {
     return (
     <div className='main container'>
-      <div className='iong'>
-        <div className='ui forum segment'>
-          感謝上師，感謝上師，感謝上師，感謝上師，感謝上師，感謝上師
+      <div className='iong ui container'>
+        <div className='ui fluid vertical menu'>
+        {
+          list.map((cate)=>
+            <div className="item">
+            <h3>{cate.category}</h3>
+            <div className="padded">
+            {cate.links.map((_link)=>
+              <p>
+              <Link to={_link.link} target="_blank">{_link.title}</Link>
+              </p>)
+            }
+            </div>
+            </div>
+            )
+        }
         </div>
       </div>
     </div>
