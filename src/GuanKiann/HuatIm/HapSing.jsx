e5e97f59dd8704fc2a0aa743a3ef4b5d40d950d9
薛丞宏 <ihcaoe@gmail.com>
2016-08-08 09:10:24 +0800

    一開始莫放音檔

--- src/GuanKiann/HuatIm/HapSing.jsx
+++ src/GuanKiann/HuatIm/HapSing.jsx
@@ -26,16 +26,14 @@ class HapSing extends React.Component {
   }
 
   play(id) {
-    debug('play', id);
     document.getElementById(id).load();
     document.getElementById(id).play();
   }
 
   render() {
-    debug('標漢字音標', this.props.標漢字音標);
     return (
       <div className='HuatIm'>
-        <audio id ={this.state.id} autoPlay>
+        <audio id ={this.state.id}>
           <source type='audio/wav'
             src={
               'http://服務.意傳.台灣/語音合成?查詢腔口=閩南語&查詢語句=' + encodeURI(this.props.標漢字音標) }
@@ -53,7 +51,6 @@ class HapSing extends React.Component {
 export default Transmit.createContainer(HapSing, {
   queries: {
     標漢字音標({ 音標 }) {
-      debug('音標', 音標);
       return (
         superagent.get('http://服務.意傳.台灣/標漢字音標?查詢腔口=閩南語&查詢語句=' + 音標)
         .then(({ body }) => (body.翻譯正規化結果))
