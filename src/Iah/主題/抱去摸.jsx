1b407fb64d19fa741da8a54dd7429dbdc51a2c26
薛丞宏 <ihcaoe@gmail.com>
2016-08-13 22:17:03 +0800

    加幾隻仔

--- src/Iah/主題/抱去摸.jsx
+++ src/Iah/主題/抱去摸.jsx
@@ -9,7 +9,9 @@ var debug = Debug('itaigi:抱去摸');
 export default class 抱去摸 extends React.Component {
 
   render() {
-    let 寶貝 = ['寶可夢', '妙蛙種子'].map((名)=>(<抱去摸一隻 key={名} 後端網址={this.props.後端網址} 寶貝名={名}/>));
+    let 寶貝 = [
+      '寶可夢', '妙蛙種子', '妙蛙草', '妙蛙花', '小火龍',
+  ].map((名)=>(<抱去摸一隻 key={名} 後端網址={this.props.後端網址} 寶貝名={名}/>));
     return (
       <div className='main container'>
         <div className='kong content'>
