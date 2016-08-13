8cfa703972e6d1df56be47a7f46eb5ba91119868
薛丞宏 <ihcaoe@gmail.com>
2016-08-13 21:57:50 +0800

    做好一个詞條矣

--- src/Iah/主題/抱去摸.jsx
+++ src/Iah/主題/抱去摸.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Transmit from 'react-transmit';
-import Promise from 'bluebird';
-var superagent = require('superagent-promise')(require('superagent'), Promise);
+import 抱去摸一隻 from './抱去摸一隻';
 
 import Debug from 'debug';
 
@@ -10,12 +9,13 @@ var debug = Debug('itaigi:抱去摸');
 export default class 抱去摸 extends React.Component {
   
   render() {
+    let 寶貝=['寶可夢','妙蛙種子'].map((名)=>(<抱去摸一隻 key={名} 後端網址={this.props.後端網址} 寶貝名={名}/>));
     return (
-    <div className='main container'>
-      <div className='kong content'>
-        XXD
+      <div className='main container'>
+        <div className='kong content'>
+           {寶貝}
+        </div>
       </div>
-    </div>
     );
   }
 }
