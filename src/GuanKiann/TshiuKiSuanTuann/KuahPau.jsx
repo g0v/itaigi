0f4780e47c4766987031671917c4a916bc38687e
Hank Wu <wutzuhan@gmail.com>
2016-08-14 21:27:40 +0800

    新增Mobile漢堡按鈕

---
+++ src/GuanKiann/TshiuKiSuanTuann/KuahPau.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+class KuahPau extends React.Component {
+
+  componentDidmount() {
+    $('.ui.sidebar').sidebar('hide');
+    $('.ui.sidebar').sidebar('setting', 'onHide', () => {
+    });
+    $('.ui.sidebar').sidebar('setting', 'onVisible', () => {
+    });
+  }
+
+  render() {
+    return (
+      <div id='KuahPau' className='ui vertical fixed icon menu'>
+        <a className='item' onClick={this._onKuahPauClick}>
+          <i className='sidebar icon'></i>
+        </a>
+      </div>
+    );
+  }
+
+  _onKuahPauClick() {
+    $('.ui.sidebar').sidebar('toggle');
+  }
+}
+
+export default KuahPau;
---
+++ src/GuanKiann/TshiuKiSuanTuann/TshiuKiSuanTuann.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TshiuKiSuanTuann extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>怎樣講</h1>
+        <h1>我很會</h1>
+        <h1>好工具</h1>
+      </div>
+    );
+  };
+}
+
+export default TshiuKiSuanTuann;
