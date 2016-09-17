50ed80d1baf0d3b527389ce6d6adacbbae2c2b02
薛丞宏 <ihcaoe@gmail.com>
2016-09-17 08:48:19 +0800

    加用愛心

--- src/Iah/Mia/Mia.jsx
+++ src/Iah/Mia/Mia.jsx
@@ -19,8 +19,30 @@ class Mia extends React.Component {
   }
   **/
 
+  貢獻程度(數量) {
+    let 心;
+    if (數量 <= 5) {
+      心 = Array.from(new Array(數量)).map((i, j)=>(
+        <i key={j} className="mini red heart icon"></i>
+      ));
+    } else if (數量 <= 13) {
+      心 = Array.from(new Array(Math.floor((數量 - 5) / 2) + 1)).map((i, j)=>(
+        <i key={j} className="small red heart icon"></i>
+      ));
+    } else if (數量 <= 33) {
+      心 = Array.from(new Array(Math.floor((數量 - 13) / 5) + 1)).map((i, j)=>(
+       <i key={j} className="large red heart icon"></i>
+     ));
+    } else {
+      心 = Array.from(new Array(Math.floor((數量) / 50) + 1)).map((i, j)=>(
+       <i key={j} className="big red heart icon"></i>
+     ));
+    }
+
+    return (<span>{心}</span>);
+  }
+
   render() {
-    debug('rendering %o', this.props.MiaData);
     return (
     <div className='main container'>
       <div className='mia ui vertical segment'>
@@ -29,7 +51,7 @@ class Mia extends React.Component {
           this.props.MiaData.內容.名人.map((g) => (
             <div className='card' key={ g.名 }>
               <div className='content'>
-                <h3>{ g.名 } （貢獻詞條數：{ g.數量 }）</h3>
+                <h3>{ g.名 }{ this.貢獻程度(+g.數量) }</h3>
                 <div className='padded'>
                   { g.詞條.map((詞)=>(
                     <button key={詞}
