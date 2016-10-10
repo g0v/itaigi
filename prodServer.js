44faa0b0a95baf8a4ba9aa62960590211db4c727
Hank Wu <wutzuhan@gmail.com>
2016-10-10 19:35:39 +0800

    怎樣講,我很會產生對應url meta tag

--- prodServer.js
+++ prodServer.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var express = require('express');
 var config = require('./webpack.config.dev');
-var template = require('./html.template');
+var template = require('./html/prodTemplate');
 
 if ((parseInt(process.versions.node[0]) == 0) && (parseInt(process.versions.node.slice(2)) <= 12)) {
   console.log(
@@ -14,13 +14,22 @@ var app = express();
 
 app.use(express.static(path.resolve('build')));
 
-app.get('/k/:Su', function (req, res) {
+app.get('/:Iah([kt])/:Su', function (req, res) {
   console.log(req.params);
-  res.send(template.render(req.params));
+  let {Iah, Su} = req.params;
+  res.send(template.render({
+    url: `http://itaigi.tw/${Iah}/${Su}`,
+    title: `${Su} - iTaigi 愛台語`,
+    image: `https://www.moedict.tw/${encodeURI(Su)}.png?font=wt064`
+  }));
 });
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send(template.render({
+    url: 'http://itaigi.tw/',
+    title: 'iTaigi 愛台語',
+    image: 'https://s3-ap-southeast-1.amazonaws.com/itaigi.tw/images/logo.png'
+  }));
 });
 
 app.listen(80, '0.0.0.0', function (err) {
