380ce6bd739cb86cfc16a8494c0f2f61331f76ec
OtomeSound <s890081tonyhsu@gmail.com>
2016-12-03 12:38:28 +0800

    Fix line wrap

--- src/Iah/Kong/無關鍵字.jsx
+++ src/Iah/Kong/無關鍵字.jsx
@@ -31,7 +31,11 @@ class 新詞區塊 extends React.Component {
           }
         )}
         <div>
-          <a className={`{ loading ? 'loading' : '' }`} onClick={this.props.onShowMoreClick}>{this.props.isShowMore ? '顯示少一點' : '顯示多一點'}</a>
+          <a
+            className={`{ loading ? 'loading' : '' }`}
+            onClick={this.props.onShowMoreClick}>
+            {this.props.isShowMore ? '顯示少一點' : '顯示多一點'}
+          </a>
         </div>
       </div>
     );
