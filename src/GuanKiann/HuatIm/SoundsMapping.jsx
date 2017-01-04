6e2108a048296e2dac7029c55045d6ea2d0188c2
a8568730 <a8568730@hotmail.com>
2017-01-04 19:19:23 +0800

    回復unorm

--- src/GuanKiann/HuatIm/SoundsMapping.jsx
+++ src/GuanKiann/HuatIm/SoundsMapping.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import mapping from './mapping.json';
+import unorm from 'unorm';
+import old_mapping from './oldMapping.json';
+
+var mapping = {};
+for (var key in old_mapping) {
+  mapping[unorm.nfc(key)] = old_mapping[key];
+}
 
 export default class SoundsMapping  {
   static map(k) {
