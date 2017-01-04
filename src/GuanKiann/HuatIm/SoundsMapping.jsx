1365e8b2fde7512267ea19b1bab8ecbeba4e9270
a8568730 <a8568730@hotmail.com>
2017-01-04 20:03:19 +0800

    比較無載入mapping.json的速度

--- src/GuanKiann/HuatIm/SoundsMapping.jsx
+++ src/GuanKiann/HuatIm/SoundsMapping.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import unorm from 'unorm';
-import old_mapping from './oldMapping.json';
+// import unorm from 'unorm';
+// import old_mapping from './oldMapping.json';
 
 var mapping = {};
-for (var key in old_mapping) {
-  mapping[unorm.nfc(key)] = old_mapping[key];
-}
+// for (var key in old_mapping) {
+//   mapping[unorm.nfc(key)] = old_mapping[key];
+// }
+
+// var mapping = old_mapping;
 
 export default class SoundsMapping  {
   static map(k) {
