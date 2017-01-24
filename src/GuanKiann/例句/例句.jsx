320692cd89a1153f2a1eec21b13d4ae67fce066d
薛丞宏 <ihcaoe@gmail.com>
2017-01-24 15:05:56 +0800

    考慮用modal

--- src/GuanKiann/例句/例句.jsx
+++ src/GuanKiann/例句/例句.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Promise from 'bluebird';
 var superagent = require('superagent-promise')(require('superagent'), Promise);
+import 例句表 from './例句表'
 import Debug from 'debug';
 
 var debug = Debug('itaigi:例句');
@@ -14,8 +15,10 @@ export default class 例句 extends React.Component {
     <span className=''>
       <button
         className='ui compact icon button'>
-        <i className='icon call'></i>
+        <i className='icon content'></i>
+        {/*<i className='icon call'></i>*/}
       </button>
+      <例句表 modalIsOpen={true} closeModal={(a)=>(a)} />
     </span>
     );
   }
---
+++ src/GuanKiann/例句/例句表.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+
+export default class 例句表 extends React.Component {
+
+  render() {
+    return (
+        <Modal
+          isOpen={this.props.modalIsOpen}
+          onRequestClose={this.props.closeModal.bind(this)}
+          >
+          <h2 ref="subtitle">已上傳！</h2>
+          <div>
+            <div>
+              事情辦得很妥善。<br/>
+              代誌做了真四序。<br/>
+              Tāi-tsì tsò-liáu tsin sù-sī. <br/>
+            </div>
+            <div>
+              事情辦得很妥善。<br/>
+              代誌做了真四序。<br/>
+              Tāi-tsì tsò-liáu tsin sù-sī. <br/>
+            </div>
+            <div>
+              事情辦得很妥善。<br/>
+              代誌做了真四序。<br/>
+              Tāi-tsì tsò-liáu tsin sù-sī. <br/>
+            </div>
+          </div>
+          <button
+            onClick={this.props.closeModal.bind(this)}
+            className="ui button"
+            >好</button>
+        </Modal>
+    );
+  }
+}
+
+
