b40337a0c44476c41d56c38bc3d01e14d72bd562
薛丞宏 <ihcaoe@gmail.com>
2017-02-08 19:18:44 +0800

    相關建議預設顯示30个

--- src/Iah/Kong/其他建議.jsx
+++ src/Iah/Kong/其他建議.jsx
@@ -7,8 +7,45 @@ import Debug from 'debug';
 var debug = Debug('itaigi:Kong其他建議');
 
 export default class 其他建議 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      全開: false,
+    };
+    this.查過的詞 = new Set();
+  }
+
+  預設顯示幾个() {
+    return 30;
+  }
+
+  看閣較濟() {
+    if (!this.state.全開 && this.props.內容.其他建議.length > this.預設顯示幾个())
+    return (
+      <div className='su card'>
+        <div className='content'>
+          <button onClick={this.看閣較濟全開.bind(this)}
+            className='ui icon button'>
+            看閣較濟
+          </button>
+        </div>
+      </div>
+      );
+  }
+
+  看閣較濟全開() {
+    this.setState({ 全開: true });
+  }
+
   render() {
-    let 文本 = this.props.內容.其他建議.map((建議, i) => (
+    let 其他建議;
+    if (this.state.全開) {
+      其他建議 = this.props.內容.其他建議;
+    } else {
+      其他建議 = this.props.內容.其他建議.slice(0, this.預設顯示幾个());
+    }
+
+    let 文本 = 其他建議.map((建議, i) => (
         <一个建議 key={i}
           後端網址={this.props.後端網址}
           文本資料={建議.文本資料} 音標資料={建議.音標資料} />
@@ -26,6 +63,7 @@ export default class 其他建議 extends React.Component {
         <div className='ui su vertical segment'>
           <div className='ui cards'>
             {文本}
+            {this.看閣較濟()}
           </div>
         </div>
       </div>
