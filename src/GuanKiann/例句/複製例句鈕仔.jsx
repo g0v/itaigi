bf57db67d7cbcb8adb3c82a0a01139672138c91c
a8568730 <a8568730@hotmail.com>
2017-03-31 15:54:49 +0800

    加上複製例句鈕仔

---
+++ src/GuanKiann/例句/複製例句鈕仔.jsx
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react';
+
+class 複製例句鈕仔 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isCopying: false };
+  }
+
+  複製例句() {
+    let { 漢字, 臺羅, 華語 } = this.props;
+    this.setState({ isCopying: true });
+    var textField = document.createElement('textarea');
+    textField.innerText = `${漢字}\n${臺羅}\n${華語}`;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+
+    let clock = setTimeout(()=> {
+        this.setState({ isCopying: false });
+      }, 3000);
+  }
+
+  render() {
+    return (
+      <span>
+        <button onClick={this.複製例句.bind(this)}
+          className="ui compact icon button">
+            <i className="copy icon"/>
+        </button>
+        { this.state.isCopying && <span>複製成功！</span> }
+      </span>
+    );
+  }
+}
+
+複製例句鈕仔.propTypes = {
+    className: PropTypes.string,
+    漢字: PropTypes.string,
+    臺羅: PropTypes.string,
+    華語: PropTypes.string,
+  };
+
+export default 複製例句鈕仔;
--- src/GuanKiann/例句/顯示例句一句.jsx
+++ src/GuanKiann/例句/顯示例句一句.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import HuatIm from '../HuatIm/HuatIm';
 import 漢字臺羅一詞 from './漢字臺羅一詞';
+import 複製例句鈕仔 from './複製例句鈕仔';
 import './顯示例句一句.css';
 
 import Debug from 'debug';
 var debug = Debug('itaigi:顯示例句一句');
 
 export default class 顯示例句一句 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     let 漢字陣列 = [];
@@ -28,6 +32,10 @@ export default class 顯示例句一句 extends React.Component {
         <p>
           <span className="ui large teal circular label">{第幾句}</span>
           <HuatIm 音標={臺羅陣列.join(' ')} />
+          <複製例句鈕仔
+            漢字={例句.漢字}
+            臺羅={例句.臺羅}
+            華語={例句.華語}/>
         </p>
         <p className='le7ku3 tai5'>{句}</p>
         <p className='le7ku3 hua5'>（{例句.華語}）</p>
