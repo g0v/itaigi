d4f1669d09baed13c20c4c328c2505a90d5173a7
Yang-Hsiang Chang <leo424y@gmail.com>
2017-12-29 09:51:22 +0800

    Make notice style as 例句複製

--- src/GuanKiann/複製連結鍵/複製連結鍵.jsx
+++ src/GuanKiann/複製連結鍵/複製連結鍵.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 
 export default class 複製連結鍵 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isCopying: false };
+  }
 
   handleClick() {
+    this.setState({ isCopying: true });
+
     var textField = document.createElement('textarea');
     textField.innerText = decodeURIComponent(window.location.href);
     document.body.appendChild(textField);
     textField.select();
     document.execCommand('copy');
     textField.remove();
-    alert('連結已複製，趕緊轉貼分享吧！');
+    // alert('連結已複製，趕緊轉貼分享吧！');
+    let clock = setTimeout(()=> {
+        this.setState({ isCopying: false });
+      }, 1000);
   }
 
   render() {
     return (
       <span className=''>
         <button
-          onClick={this.handleClick}
+          onClick={this.handleClick.bind(this)}
           className='ui compact icon button' title='複製連結'>
           <i className='icon linkify'></i>
         </button>
+        { this.state.isCopying &&
+          <span><small>連結已複製，可轉貼分享</small></span>
+        }
       </span>
     );
   }
