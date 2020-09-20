1314bc759f4406f7bfa4bfbcf61ccf29199adc2d
James Hsu <jameshsu1125@gmail.com>
2020-09-20 16:21:19 +0800

    fix document format for jscs
    ESLint謀甘單

--- src/Ogilvy/主題/寶鑑按鈕.js
+++ src/Ogilvy/主題/寶鑑按鈕.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class 寶鑑按鈕 extends React.Component {
-	render() {
-		return (
-			<div className='台語寶鑑-col'>
-	 			<Link to={ this.props.data.to }>
-		 			<div className='台語寶鑑-button'>
-		 				<div className='台語寶鑑-button-ico' style={{ backgroundImage:`url(${ this.props.data.image })` }}></div>
-		 				<div className='台語寶鑑-button-name'>{ this.props.data.name }</div>
-		 			</div>
-	 			</Link>
-	 		</div>
-		);
-	}
+  render()
+  {
+    let 按鈕 = this.props.data.image;
+    return (
+      <div className='台語寶鑑-col'>
+        <Link to={ this.props.data.to }>
+          <div className='台語寶鑑-button'>
+            <div className='台語寶鑑-button-ico' style={{ backgroundImage: `url(${按鈕})` }}></div>
+            <div className='台語寶鑑-button-name'>{ this.props.data.name }</div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
 }
 
