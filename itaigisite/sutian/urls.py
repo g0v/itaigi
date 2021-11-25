aa39653ad7c2d8d67e61f4add92ec84e7e375580
a8568730 <a8568730@hotmail.com>
2021-11-25 15:39:14 +0800

    + 查詢辭典頁。

--- itaigisite/sutian/urls.py
+++ itaigisite/sutian/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from sutian.views import tiann, tshiau
 
 urlpatterns = [
-    path('k/', tshiau, name='tshiau'),
+    path('k/<str:ji>', tshiau, name='tshiau'),
     path('', tiann, name='tiann'),
 ]
--- itaigisite/sutian/views.py
+++ itaigisite/sutian/views.py
@@ -7,5 +7,5 @@ def tiann(request):
     return render(request, 'sutian/tiann.html')
 
 
-def tshiau(request):
-    return render(request, 'sutian/tiann.html')
+def tshiau(request, ji):
+    return render(request, 'sutian/tshiau.html')
