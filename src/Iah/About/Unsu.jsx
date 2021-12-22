8624e1884492a12bf5cd027c4f01ba121b89872b
a8568730 <a8568730@hotmail.com>
2021-12-22 09:59:49 +0800

    *  隱私頭前加TOC頁面索引。

--- src/Iah/About/Unsu.jsx
+++ src/Iah/About/Unsu.jsx
@@ -11,13 +11,29 @@ class Unsu extends React.Component {
 
 <p>iTaigi公民華台辭典（以下簡稱iTaigi）作為民眾查詢以及交流台語語詞之非盈利開源網站，iTaigi絕對遵守個人資料保護法及相關法令之規定，保護用戶的個人隱私權。為了協助您清楚地瞭解iTaigi如何蒐集、應用及保護您所提供的個人資料，請您詳細閱讀iTaigi隱私權保護政策。 </p>
 
+<nav>
+  <b>條款索引</b>
+  <ul>
+    <li><a href="#huanui">適用範圍</a></li>
+    <li><a href="#luihing">蒐集的資料類型、蒐集目的及使用方式</a></li>
+    <li><a href="#iah">個人資料分享政策</a></li>
+    <li><a href="#hoo">個人資料保護政策</a></li>
+    <li><a href="#kikan">個人資料使用期間與地區</a></li>
+    <li><a href="#cookie">Cookie的運用</a></li>
+    <li><a href="#kati">自我保護措施</a></li>
+    <li><a href="#siu">隱私權保護政策修訂</a></li>
+    <li><a href="#tsiohmng">與我們聯絡</a></li>
+    
+  </ul>
+</nav>
+
 <section className="margin-b-2">
-    <h2>適用範圍</h2>
+    <h2 id="huanui">適用範圍</h2>
     <p>本隱私權保護政策適用於您在使用iTaigi過程中，涉及個人資料蒐集、處理、利用與保護之相關作法。不適用於iTaigi以外的相關連結網站，也不適用於非iTaigi所委託或參與管理的人員。</p>
 </section>
 
 <section className="margin-b-2">
-    <h2>蒐集的資料類型、蒐集目的及使用方式</h2>
+    <h2 id="luihing">蒐集的資料類型、蒐集目的及使用方式</h2>
     <h3>帳戶資料</h3>
     <p>當您於iTaigi註冊成為會員時，我們會詢問您的姓名、電子郵件這兩項資料。您在註冊成為會員並登入各項服務後，我們就能識別您的身分並提供您回答他人問題、投票、收藏等線上服務以及整體統計。</p>
     <p>倘若您如不提供姓名、電子郵件這兩項資料，則無法使用相關服務。您的電子郵件地址會連結到您的統計資料和互動資料，不會公開分享。您隨時都可以刪除帳戶，刪除後系統會移除您的使用者名稱和電子郵件以及個人統計數量，並將您回答的台語詞由具名改做匿名。</p>
@@ -30,18 +46,18 @@ class Unsu extends React.Component {
 </section>
 
 <section className="margin-b-2">
-<h2>個人資料分享政策</h2>
+<h2 id="iah">個人資料分享政策</h2>
 <p>iTaigi將依個人資料保護法及相關法令之規定，只就其iTaigi之用，不會任意對其他第三者揭開販售。除非司法單位因公眾安全，經合法正式的程序要求iTaigi公開特定個人資料時，以及對iTaigi所有使用者安全考量下做必要的配合。</p>
 </section>
 
 <section className="margin-b-2">
-<h2>個人資料保護政策</h2>
+<h2 id="hoo">個人資料保護政策</h2>
 <p>iTaigi網站主機均設有防火牆等相關的各項資訊安全設備及必要的安全防護措施，加以保護網站及您的個人資料。
 採用嚴格的保護措施，只由經過授權的人員才能接觸您的個人資料。</p>
 </section>
 
 <section className="margin-b-2">
-<h2>個人資料使用期間與地區</h2>
+<h2 id="kikan">個人資料使用期間與地區</h2>
 <p>iTaigi僅在臺灣境內利用您的個人資料。您在iTaigi所輸入的個人資料，除於服務項目網頁上另有公告者，將因應前述蒐集目的永久保存；惟iTaigi其它因服務維運作業需要，擁有刪除資料之權利。</p>
 
 <p>當您在iTaigi中註冊成為會員後，您可以隨時利用您的帳號和密碼登入iTaigi「個人頁面」功能，對您填寫的會員個人資料，進行查閱、製給複本及刪除。請注意，在您選擇刪除所註冊之會員帳號後，您將無法再以該帳號登入iTaigi，其餘個人資料權利之行使，您可透過電話或線上申請方式向iTaigi客服中心提出。</p>
@@ -50,9 +66,8 @@ class Unsu extends React.Component {
 </section>
 
 <section className="margin-b-2">
-<h2>Cookie的運用</h2>
+<h2 id="cookie">Cookie的運用</h2>
 <p>Cookies為資訊系統識別使用者而記錄於使用者硬碟之簡短資訊，下列情況，iTaigi會寫入及讀取Cookie：</p>
-
 <ul>
     <li>在您為了進行身分認證而使用「單一登入」服務時。</li>
     <li>為統計iTaigi瀏覽人數、使用Google Analytics分析用戶瀏覽模式及瀏覽網頁的情況，做為iTaigi改善服務的參考。</li>
@@ -62,17 +77,17 @@ class Unsu extends React.Component {
 </section>
 
 <section className="margin-b-2">
-<h2>自我保護措施</h2>
+<h2 id="kati">自我保護措施</h2>
 <p>請妥善保管您的密碼或任何個人資料，不要將任何個人資料，尤其是密碼提供給任何人。在您登入系統完成各項作業後，務必記得登出帳號，若您是與他人共享電腦或使用公共電腦，切記要關閉瀏覽器視窗，以防止他人讀取您的個人資料、信件或進入iTaigi。</p>
 </section>
 
 <section className="margin-b-2">
-<h2>隱私權保護政策修訂</h2>
+<h2 id="siu">隱私權保護政策修訂</h2>
 <p>本隱私權保護政策將不定期修訂。iTaigi不會在未經您明確同意的情況下，縮減您的權利。隱私權保護政策變更時一律於iTaigi以及iTaigi官方社群帳號發布。另外，iTaigi舊版隱私權保護政策也將留存，方便您回顧。</p>
 </section>
 
 <section className="margin-b-2">
-<h2>與我們聯絡</h2>
+<h2 id="tsiohmng">與我們聯絡</h2>
 若您對iTaigi隱私權保護政策或個人資料行使權利有任何疑問，請與iTaigi維護團隊聯繫，聯絡方式如下: 
 <ul>
     <li>維護團隊：意傳科技有限公司</li>
